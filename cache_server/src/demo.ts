09bac5abd19aa1fc573aa214860c95c01616d735
huwenchao <huwenchao@cryptape.com>
2019-12-26 15:54:56 +0800

    remove cache server

--- cache_server/src/demo.ts
+++
@@ -1,6 +0,0 @@
-import { graphql } from "graphql";
-import { readFileSync } from "fs";
-import { schema } from './schema';
-
-const query = readFileSync("schemas/query.graphql", "utf-8");
-graphql(schema, query).then((result) => console.log(JSON.stringify(result, null, 2)));
--- cache_server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import { schema } from './schema';
-
-const server = new ApolloServer({
-  schema
-});
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
