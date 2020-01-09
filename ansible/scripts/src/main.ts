a1f2c54dd49eb4c6745c0ce958dd18e41320061b
huwenchao <huwenchao@cryptape.com>
2020-01-09 15:27:38 +0800

    fix start binary chain

--- ansible/scripts/src/main.ts
+++ ansible/scripts/src/main.ts
@@ -5,5 +5,5 @@ import { log, CHAIN_META } from "./utils";
 
 log("start_scripts", { chain: CHAIN_META.chain_id }, "info", true);
 setInterval(monitor, 5 * 60 * 1000);
-setInterval(randomChaos, 5 * 60 * 1000);
+// setInterval(randomChaos, 5 * 60 * 1000);
 setInterval(tx_sender, 1000);
