9a97538bd6405365998a972621b6b3bf13d8240c
zhengjianhui <815168733@qq.com>
2022-01-12 14:24:44 +0800

    Update contract deploy script

--- uni-contract-deploy/templates/deploy.js
+++ uni-contract-deploy/templates/deploy.js
@@ -1,5 +1,7 @@
 const Web3 = require('web3')
 const ethers = require("ethers");
+const fs = require('fs');
+const yaml = require('js-yaml');
 const WETH9 = require('./build/WETH9.json')
 const UniswapV2Pair = require('./build/UniswapV2Pair.json')
 const UniswapV2Factory = require('./build/UniswapV2Factory.json')
@@ -13,10 +15,8 @@ async function sendTransaction(web3, chainId, account, data, nonce, gasPrice) {
     const tx = {
         type: 2,
         nonce: nonce,
-        // to: "0x8D97689C9818892B700e27F316cc3E41e17fBeb9", // Address to send to
-        maxPriorityFeePerGas: 215573697772, // Recommended maxPriorityFeePerGas
-        maxFeePerGas: 215573697772, // Recommended maxFeePerGas
-        // value: ethers.utils.parseEther("0.01"), // .01 ETH
+        maxPriorityFeePerGas: 250, // Recommended maxPriorityFeePerGas
+        maxFeePerGas: 250, // Recommended maxFeePerGas
         gasLimit: web3.utils.stringToHex("21000"), // basic transaction costs exactly 21000
         chainId: 5, // Ethereum network id
         data: data
@@ -34,49 +34,69 @@ async function sendTransaction(web3, chainId, account, data, nonce, gasPrice) {
     const gasPrice = await web3.eth.getGasPrice()
     let nonce = await web3.eth.getTransactionCount(account.address) + 1
 
+    let contract_address = {
+        WETH: '',
+        WETH_TX_HASH: "",
+        UniswapV2Factory: '',
+        UniswapV2Factory_TX_HASH: '',
+        UniswapV2Router01:'',
+        UniswapV2Router01_TX_HASH:'',
+        UniswapV2Router02: '',
+        UniswapV2Router02_TX_HASH: '',
+        InitCodeHash:''
+        
+    }
+
     // deploy WETH contract
-    let weth = null
     {
         const contract = new web3.eth.Contract(WETH9.abi)
         const data = contract.deploy({ data: WETH9.bytecode }).encodeABI()
         const receipt = await sendTransaction(web3, chainId, account, data, nonce, gasPrice)
-        console.info('WETH:', weth = receipt.contractAddress)
         nonce = nonce + 1
+        contract_address.WETH = receipt.contractAddress
+        contract_address.WETH_TX_HASH = receipt.transactionHash
     }
 
 
     // deploy UniswapV2Factory contract
-    let factory = null
     {
         const contract = new web3.eth.Contract(UniswapV2Factory.abi)
         const options = { data: UniswapV2Factory.bytecode, arguments: [account.address] }
         const data = contract.deploy(options).encodeABI()
         const receipt = await sendTransaction(web3, chainId, account, data, nonce, gasPrice)
-        console.info('UniswapV2Factory:', factory = receipt.contractAddress)
         nonce = nonce + 1
+        contract_address.UniswapV2Factory = receipt.contractAddress
+        contract_address.UniswapV2Factory_TX_HASH = receipt.transactionHash
     }
 
     // deploy UniswapV2Router01 contract
     {
         const contract = new web3.eth.Contract(UniswapV2Router01.abi)
-        const options = { data: UniswapV2Router01.bytecode, arguments: [factory, weth] }
+        const options = { data: UniswapV2Router01.bytecode, arguments: [contract_address.UniswapV2Factory, contract_address.WETH] }
         const data = contract.deploy(options).encodeABI()
         const receipt = await sendTransaction(web3, chainId, account, data, nonce, gasPrice)
-        console.info('UniswapV2Router01:', receipt.contractAddress)
         nonce = nonce + 1
+        contract_address.UniswapV2Router01 = receipt.contractAddress
+        contract_address.UniswapV2Router01_TX_HASH = receipt.transactionHash
     }
 
     // deploy UniswapV2Router02 contract
     {
         const contract = new web3.eth.Contract(UniswapV2Router02.abi)
-        const options = { data: UniswapV2Router02.bytecode, arguments: [factory, weth] }
+        const options = { data: UniswapV2Router02.bytecode, arguments: [contract_address.UniswapV2Factory, contract_address.WETH] }
         const data = contract.deploy(options).encodeABI()
         const receipt = await sendTransaction(web3, chainId, account, data, nonce, gasPrice)
-        console.info('UniswapV2Router02:', receipt.contractAddress)
         nonce = nonce + 1
+        contract_address.UniswapV2Router02 = receipt.contractAddress
+        contract_address.UniswapV2Router02_TX_HASH = receipt.transactionHash
     }
 
     let data = UniswapV2Pair.bytecode
     if (!data.startsWith('0x')) data = '0x' + data
-    console.info('INIT_CODE_HASH:', web3.utils.keccak256(data))
+    contract_address.InitCodeHash =  web3.utils.keccak256(data)
+
+    console.log(contract_address);
+
+    let yamlStr = yaml.dump(contract_address);
+    fs.writeFileSync('contract_address.yaml', yamlStr, 'utf8');
 })()
