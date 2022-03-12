3927cfdc3c9b7e868b1f82912a15258772890fd7
DavidChild <844305358@qq.com>
2022-03-12 23:41:08 +0800

    feat(uni-v3): add deploy uni-v3 contract and interface (#14)
    * feat(uni-v3): add deploy uni-v3 contract and interface
    
    * update code

--- uni-test/scripts/test-uniswap-script.js
+++ uni-test/scripts/test-uniswap-script.js
@@ -5,98 +5,128 @@
 // Runtime Environment's members available in the global scope.
 require("@nomiclabs/hardhat-waffle");
 const hre = require("hardhat");
-;
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   // We get the contract to deploy
-  const { expect } = require("chai")
   const accounts = await hre.ethers.getSigners();
-  const account_a = accounts[0];
-  const account_b = accounts[1];
+  const account1 = accounts[0];
+  const account2 = accounts[1];
 
-  //get  facatory constract objcet 
-  const Facatory = await hre.ethers.getContractFactory("UniswapV2Factory");
-  //get  weth constract objcet 
-  const WETH = await hre.ethers.getContractFactory("WETH9");
-  //get  UniswapV2Router02 constract objcet
-  const Router02 = await hre.ethers.getContractFactory("UniswapV2Router02");
-  //get one Token ERC20  that name is TT
-  const ERC20_1 = await hre.ethers.getContractFactory("TRAMS");
-  //get one Token ERC20  tha name is  TS
-  const ERC20_2 = await hre.ethers.getContractFactory("TRAMS");
-  //get one Token ERC20  tha name is  TSY
-  const ERC20_3 = await hre.ethers.getContractFactory("TRAMS");
+  // get  facatory contract objcet
+  const facatory = await hre.ethers.getContractFactory("UniswapV2Factory");
+  // get  weth contract objcet
+  const weth = await hre.ethers.getContractFactory("WETH9");
+  // get  UniswapV2Router02 contract objcet
+  const router02 = await hre.ethers.getContractFactory("UniswapV2Router02");
+  // get one Token ERC20  that name is TT
+  const erc201 = await hre.ethers.getContractFactory("TRAMS");
+  // get one Token ERC20  tha name is  TS
+  const erc202 = await hre.ethers.getContractFactory("TRAMS");
+  // get one Token ERC20  tha name is  TSY
+  const erc203 = await hre.ethers.getContractFactory("TRAMS");
 
-  //publish the factory constract 
-  const Facatory_de = await Facatory.deploy(account_a.address);
-  Facatory_de.deployed();
-  console.log("facatory address is ", Facatory_de.address)
+  // publish the factory contract
+  const facatoryDe = await facatory.deploy(account1.address);
+  facatoryDe.deployed();
+  console.log("facatory address is ", facatoryDe.address);
 
-  //publish the weth constract 
-  const Weth_de = await WETH.deploy();
-  Weth_de.deployed();
-  console.log("weth address is ", Weth_de.address)
+  // publish the weth contract
+  const wethDe = await weth.deploy();
+  wethDe.deployed();
+  console.log("weth address is ", wethDe.address);
 
+  // publish contract UniswapV2Router02
+  const router02De = await router02.deploy(
+    facatoryDe.address,
+    wethDe.address,
+  );
+  router02De.deployed();
+  console.log("UniswapV2Router02 address is ", router02De.address);
 
-  //publish constract UniswapV2Router02
-  const Router02_de = await Router02.deploy(Facatory_de.address, Weth_de.address);
-  Router02_de.deployed();
-  console.log("UniswapV2Router02 address is ", Router02_de.address)
+  // deploy TT,TS,TSY Tokens
+  const erc201De = await erc201.deploy("TT", "TT1", account1.address);
+  const erc202De = await erc202.deploy("TS", "TS1", account1.address);
+  const erc203De = await erc203.deploy("TSY", "TSY", account2.address);
 
-  //deploy TT,TS,TSY Tokens
-  const ERC20_1_de = await ERC20_1.deploy("TT", "TT1", account_a.address);
-  const ERC20_2_de = await ERC20_2.deploy("TS", "TS1", account_a.address);
-  const ERC20_3_de = await ERC20_3.deploy("TSY", "TSY", account_b.address);
+  erc201De.deployed();
+  erc202De.deployed();
+  erc203De.deployed();
 
-  ERC20_1_de.deployed();
-  ERC20_2_de.deployed();
-  ERC20_3_de.deployed();
+  // checked the  status of ERC20 is deployed
+  console.log("erc201De address is ", erc201De.address);
+  console.log("erc201De name is ", await erc201De.name());
 
+  // create uniswap a token pari
+  const pairCreateTx = await facatoryDe.createPair(
+    erc201De.address,
+    erc202De.address,
+  );
+  await pairCreateTx.wait();
+  const pairAddress = await facatoryDe.getPair(
+    erc201De.address,
+    erc202De.address,
+  );
+  console.log("pairAddress is:", pairAddress);
 
-  //checked the  status of ERC20 is deployed 
-  console.log("ERC20_1_de address is ", ERC20_1_de.address);
-  console.log("ERC20_1_de name is ", await ERC20_1_de.name());
-
-  //create uniswap a token pari
-  const pair_create_tx = await Facatory_de.createPair(ERC20_1_de.address, ERC20_2_de.address);
-  await pair_create_tx.wait();
-  const pair_address = await Facatory_de.getPair(ERC20_1_de.address, ERC20_2_de.address);
-  console.log("pair_address is:", pair_address);
-
-
-  //approve TT,TS,TSY token into the UniswapV2Router02 address
-  console.log("account_a for TT  allowance is:", await ERC20_1_de.allowance(account_a.address, account_b.address));
-  const p1 = await ERC20_1_de.approve(Router02_de.address, 100000000000);
-  const p2 = await ERC20_2_de.approve(Router02_de.address, 100000000000);
-  const p3 = await ERC20_3_de.approve(Router02_de.address, 100000000000);
+  // approve TT,TS,TSY token into the UniswapV2Router02 address
+  console.log(
+    "account1 for TT  allowance is:",
+    await erc201De.allowance(account1.address, account2.address),
+  );
+  const p1 = await erc201De.approve(router02De.address, 100000000000);
+  const p2 = await erc202De.approve(router02De.address, 100000000000);
+  const p3 = await erc203De.approve(router02De.address, 100000000000);
   await p1.wait();
   await p2.wait();
   await p3.wait();
-  console.log("account_a for TT  allowance is:", await ERC20_1_de.allowance(account_a.address, account_b.address));
+  console.log(
+    "account1 for TT  allowance is:",
+    await erc201De.allowance(account1.address, account2.address),
+  );
 
-  //test addLiquidity
-  const addliq = await Router02_de.addLiquidity(ERC20_1_de.address, ERC20_2_de.address, 10000000000, 10000000000, 10000000, 10000000, account_a.address, 1642216356000);
+  // test addLiquidity
+  await router02De.addLiquidity(
+    erc201De.address,
+    erc202De.address,
+    10000000000,
+    10000000000,
+    10000000,
+    10000000,
+    account1.address,
+    1642216356000,
+  );
 
-
-  //if you test  UniswapV2Router02.addLiquidityETH,the  the  msg.sender have weth value that is not null,you must inject some money into weth token.
-  const pledgeEth = await Weth_de.deposit({ value: 49000000 });
+  /*
+  if you test  UniswapV2Router02.addLiquidityETH,
+  the  the  msg.sender have weth value that is not null,you
+  must inject some money into weth token.
+  */
+  const pledgeEth = await wethDe.deposit({ value: 49000000 });
   await pledgeEth.wait();
 
-  //"0xc6ff18494dd8fe6b7df18b9921bac2a5af650377" is uniswap created auto  ,  pari of tokan a is eth,token b is anther token. Note: the address  you can find the console log.
-  const pledge_eth_transfer = await Weth_de.transfer("0xc6ff18494dd8fe6b7df18b9921bac2a5af650377", 40000);
-  await pledge_eth_transfer.wait();
+  /*
+  "0xc6ff18494dd8fe6b7df18b9921bac2a5af650377"
+  is uniswap created auto  ,
+  pari of tokan a is eth,token b is anther token.
+  Note: the address  you can find the console log.
+  */
+  const pledgeEthTransfer = await wethDe.transfer(
+    "0xc6ff18494dd8fe6b7df18b9921bac2a5af650377",
+    40000,
+  );
+  await pledgeEthTransfer.wait();
 
-  //test addLiquidityETH
-  const addliqEth = await Router02_de.addLiquidityETH(ERC20_1_de.address, 10000000000, 10000000000, 10000000, account_a.address, 1642216356000);
-  const receipt_liq = await addliqEth.wait();
-  console.log("receipt is ", receipt_liq);
-
+  // test addLiquidityETH
+  const addliqEth = await router02De.addLiquidityETH(
+    erc201De.address,
+    10000000000,
+    10000000000,
+    10000000,
+    account1.address,
+    1642216356000,
+  );
+  const receiptLiq = await addliqEth.wait();
+  console.log("receipt is ", receiptLiq);
 }
 main()
   .then(() => process.exit(0))
