6d1b8bb08e338238e19339917f4a5f279af91bea
zhengjianhui <815168733@qq.com>
2022-03-31 19:17:00 +0800

    feat(pr-bot): impl pr bot (#34)
    * feat(pr-bot): impl pr bot
    
    * fmt
    
    * Fix hour to millisecond conversion error
    
    * fmt
    
    * fmt

---
+++ pr-bot/index.js
@@ -0,0 +1,25 @@
+const RepoInfo = require('./src/repo/repo_info');
+const DiscordWebhook = require('./src/discord/discord_webhook');
+const config = require('./config.json');
+
+(async function () {
+    setInterval(async () => {
+        try {
+            const hours = new Date().getHours();
+            if (hours > 19 || hours < 9) {
+                return;
+            }
+
+            const discord = new DiscordWebhook(config);
+            for (const index in config.repos) {
+                const repo_info = new RepoInfo(config.repos[index].onwer, config.repos[index].repo);
+                const res = await repo_info.getPrInfos();
+                await discord.sendPrInfos(res);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+
+    }, config.time_interval_for_notification * 60 * 60 * 1000);
+
+})();
