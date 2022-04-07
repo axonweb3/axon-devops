bc7de518a74b0d1aa2be037a38eb5f8204234a8f
wenyuanhust <wenyuanhust@gmail.com>
2022-04-07 15:24:08 +0800

    axon(cli): start axon by axon_cli

--- axon-cli/src/interactive.rs
+++ axon-cli/src/interactive.rs
@@ -1,5 +1,6 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use std::process::Command;  // 引入命令模块
 
 pub fn interactive() {
     // `()` can be used when no completer is required
@@ -13,6 +14,19 @@ pub fn interactive() {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
                 println!("Line: {}", line);
+                if line == "start" {
+                    let output = Command::new("/home/wenyuan/git/axon/target/debug/axon")
+                    .arg("--config=/home/wenyuan/git/axon/devtools/chain/config.toml")
+                    .arg("--genesis=/home/wenyuan/git/axon/devtools/chain/genesis_single_node.json")
+                    .output().expect("执行异常，提示");
+                    // let output = Command::new("ls").arg("-l").output().expect("执行异常，提示");
+                    let ls_list = String::from_utf8(output.stdout);
+                    println!("{}", ls_list.unwrap());
+                    break;
+                } else {
+                    println!("please input the right command!");
+                }
+
             }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
