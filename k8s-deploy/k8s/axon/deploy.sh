793a91bec582bcaafbde40180667403f9d0c9552
Genysys <112424909+samtvlabs@users.noreply.github.com>
2022-10-14 17:53:01 +0400

    chore: updating configs

--- k8s-deploy/k8s/axon/deploy.sh
+++ k8s-deploy/k8s/axon/deploy.sh
@@ -3,7 +3,7 @@
 function clean() {
     set +e
     echo "DEBUG" "Clean env to delete old axons, please wait..."
-
+    
     (kubectl get sts -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^axon" || true) | while read -r name; do
         kubectl delete sts "$name" -n "axon">/dev/null 2>&1
     done
@@ -17,30 +17,31 @@ function clean() {
 
 function create_configmap() {
     echo "DEBUG" "create configmap for axons, please wait..."
-    kubectl create configmap node1-toml --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/node_1.toml -n axon
-    kubectl create configmap node2-toml --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/node_2.toml -n axon
-    kubectl create configmap node3-toml --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/node_3.toml -n axon
-    kubectl create configmap node4-toml --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/node_4.toml -n axon
-    kubectl create configmap genesis --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/genesis.json -n axon
-    kubectl create configmap db-options --from-file=/home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-config/default.db-options -n axon
+    kubectl create namespace axon --dry-run=client -o yaml | kubectl apply -f -
+    kubectl create configmap node1-toml --from-file=./axon-config/node_1.toml -n axon
+    kubectl create configmap node2-toml --from-file=./axon-config/node_2.toml -n axon
+    kubectl create configmap node3-toml --from-file=./axon-config/node_3.toml -n axon
+    kubectl create configmap node4-toml --from-file=./axon-config/node_4.toml -n axon
+    kubectl create configmap genesis --from-file=./axon-config/genesis.json -n axon
+    kubectl create configmap db-options --from-file=./axon-config/default.db-options -n axon
 }
 function deploy_axon(){
     echo "DEBUG" "deploy axons, please wait..."
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/axon/axon1-statefulset.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/axon/axon2-statefulset.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/axon/axon3-statefulset.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/axon/axon4-statefulset.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/axon/axon-chain.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/ingress/axon-ingress.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/pv/axon-logs-pv.yaml
-    kubectl apply -f /home/ckb/axon-devops/k8s-deploy/k8s/pv/axon-logs-pvc.yaml
+    kubectl apply -f ./axon1-statefulset.yaml
+    kubectl apply -f ./axon2-statefulset.yaml
+    kubectl apply -f ./axon3-statefulset.yaml
+    kubectl apply -f ./axon4-statefulset.yaml
+    kubectl apply -f ./axon-chain.yaml
+    kubectl apply -f ../ingress/axon-ingress.yaml
+    kubectl apply -f ../pv/axon-logs-pv.yaml
+    kubectl apply -f ../pv/axon-logs-pvc.yaml
     echo "DEBUG" "waiting for axons running..."
     sleep 120
     axon_running_num=`kubectl get pod -n axon | grep -i "axon" |grep -ci "running"`
     if [[ $axon_running_num -lt 4 ]]; then
-       echo "axons not stable,please have a check"
+        echo "axons not stable,please have a check"
     else
-       echo "axons running"
+        echo "axons running"
     fi
 }
 
@@ -50,17 +51,17 @@ function main() {
         "deploy")
             create_configmap
             deploy_axon
-            ;;
+        ;;
         "deploy_axon")
             deploy_axon
-            ;;
+        ;;
         "create_configmap")
             create_configmap
-            ;;
+        ;;
         "clean")
             clean
-            ;;
-        esac
+        ;;
+    esac
 }
 
 main $*
