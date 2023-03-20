cf6b4d3eda6e262ce870227380339c76b8466425
liya2017 <Liya@cryptape.com>
2023-03-20 16:41:41 +0800

    refactor: filter out the pr that created by dependbot

--- pr-bot/index.js
+++ pr-bot/index.js
@@ -1,23 +1,23 @@
 const config = require('./config.json')
+const cron = require('node-cron');
 const DiscordWebhook = require('./src/discord/discord_webhook')
 const RepoInfo = require('./src/repo/repo_info')
 process.env.TZ = 'Asia/Shanghai'
 ;(async function () {
-	setInterval(async () => {
-		try {
-			const hours = new Date().getHours()
-			if (hours > 19 || hours < 9) {
-				return
+		cron.schedule('30 09 * * *', async () => {
+			try {
+				const hours = new Date().getHours()
+				if (hours > 19 || hours < 9) {
+					return
+				}
+				const discord = new DiscordWebhook(config)
+				for (const index in config.repos) {
+					const repo_info = new RepoInfo(config.repos[index].onwer, config.repos[index].repo)
+					const res = await repo_info.getPrInfos()
+					await discord.sendPrInfos(res)
+				}
+			} catch (e) {
+				console.log(e)
 			}
-
-			const discord = new DiscordWebhook(config)
-			for (const index in config.repos) {
-				const repo_info = new RepoInfo(config.repos[index].onwer, config.repos[index].repo)
-				const res = await repo_info.getPrInfos()
-				await discord.sendPrInfos(res)
-			}
-		} catch (e) {
-			console.log(e)
-		}
-	}, config.time_interval_for_notification * 60 * 60 * 1000)
+		});
 })()
